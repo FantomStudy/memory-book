707165b11ce483a2d6b4ca10a2b580efe9f7baec
visstt <egorskomorohov020606@gmail.com>
2025-02-14 00:46:13 +0500

    add registration

--- src/Pages/Auth/Registration/Registration.jsx
+++ src/Pages/Auth/Registration/Registration.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import useAuthStore from "../../../store/authStore";
 import styles from "./Registration.module.css";
 import { Container } from "@mui/material";
+import { Link } from "react-router-dom";
+import ActivePeople from "../../../../public/icons/logoActivePeople.svg";
+
 export default function Registration() {
   const {
     login,
@@ -28,8 +31,9 @@ export default function Registration() {
         alignItems: "center",
       }}
     >
-      <h1>Зарегистрироваться</h1>
       <form onSubmit={handleSubmit} className={styles.reg_form}>
+        <h1>Зарегистрироваться</h1>
+
         <input
           className={styles.reg_input}
           type="text"
@@ -73,9 +77,21 @@ export default function Registration() {
         />
 
         <button type="submit" className={styles.submit_button}>
-          submit
+          Зарегистрироваться
         </button>
       </form>
+      <p className={styles.reg_description}>
+        У вас есть аккаунт? <Link>Войти</Link>
+      </p>
+      <b className={styles.chose}>Или</b>
+
+      <div className={styles.active_people}>
+        <img src={ActivePeople} alt="ActivePeople" />
+        <div className={styles.text_container}>
+          <h3>Активный гражданин</h3>
+          <h3 className={styles.thin}>Оренбургской области</h3>
+        </div>
+      </div>
     </Container>
   );
 }
